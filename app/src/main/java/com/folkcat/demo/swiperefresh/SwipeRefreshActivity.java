20a1971330a3efdedc22181637a8d6ad7c6ca61b
tamas1994 <donghaifeng@hotmail.com>
2016-04-23 05:24:25 +0800

    Add swipe refresh demo

---
+++ app/src/main/java/com/folkcat/demo/swiperefresh/SwipeRefreshActivity.java
@@ -0,0 +1,68 @@
+package com.folkcat.demo.swiperefresh;
+
+/**
+ * Created by Tamas on 2016/4/23.
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.folkcat.demo.R;
+
+public class SwipeRefreshActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private static final String TAG="SwipeRefreshActivity";
+
+    private SwipeRefreshLayout mSwipeLayout;
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
+    private List<String> mDatas = new ArrayList<String>(Arrays.asList("Java", "Javascript", "C++", "Ruby", "Json",
+            "HTML"));
+
+
+
+    @SuppressLint("InlinedApi")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_refresh);
+
+        mListView = (ListView) findViewById(R.id.id_listview);
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.id_swipe_ly);
+
+        mSwipeLayout.setOnRefreshListener(this);
+        mSwipeLayout.setColorScheme(android.R.color.holo_blue_bright, android.R.color.holo_green_light,
+                android.R.color.holo_orange_light, android.R.color.holo_red_light);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDatas);
+        mListView.setAdapter(mAdapter);
+
+    }
+
+    public void onRefresh() {
+        Log.i(TAG, "onRefresh is calling");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for(int i=0;i<8;i++){
+                    mDatas.add(i,"item:"+i);
+                }
+                mAdapter.notifyDataSetChanged();
+                mSwipeLayout.setRefreshing(false);
+
+            }
+        }).start();
+
+    }
+}
