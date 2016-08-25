e2e8031182aba9900301a0ed85eaa704ba731de6
tamas1994 <donghaifeng@hotmail.com>
2016-08-25 14:03:52 +0800

    修改Gradle版本为2.1.3

--- app/src/main/java/com/folkcat/demo/MainActivity.java
+++ app/src/main/java/com/folkcat/demo/MainActivity.java
@@ -1,43 +1,57 @@
 package com.folkcat.demo;
 
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
+import android.util.Log;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
     private static final String TAG="MainActivity";
+    private TextView mTvLoad;
+    private Handler mHandler;
+
 
     //myObservable.subscribe(onNextAction);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mTvLoad=(TextView)findViewById(R.id.tv_load);
+        mHandler=new Handler();
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onStart(){
+        super.onStart();
 
+
+
+    }
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onResume(){
+        super.onResume();
+        Log.i(TAG,"onResume Called");
+        //耗时操作
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTvLoad.setText("加载完成");
+                    }
+                });
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            }
+        }).start();
 
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
