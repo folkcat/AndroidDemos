84a8fa04355e519836879c615b816b48bc4fd8a2
tamas1994 <donghaifeng@hotmail.com>
2016-10-13 11:36:16 +0800

    完善XMPP Demo

--- app/src/main/java/com/folkcat/demo/xmpp/TestXMPPActivity.java
+++ app/src/main/java/com/folkcat/demo/xmpp/TestXMPPActivity.java
@@ -3,6 +3,8 @@ package com.folkcat.demo.xmpp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import com.folkcat.demo.R;
 
@@ -13,27 +15,68 @@ import org.jivesoftware.smack.XMPPException;
 /**
  * Created by Tamas on 2016/9/18.
  */
-public class TestXMPPActivity extends Activity {
+public class TestXMPPActivity extends Activity implements View.OnClickListener{
     private static final String TAG="TestXMPPActivity";
 
+    private EditText mEtUserName;
+    private EditText mEtPasswd;
+    private XMPPManager mXMPPManager;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_xmpp);
+        mEtUserName=(EditText)findViewById(R.id.et_xmpp_username);
+        mEtPasswd=(EditText)findViewById(R.id.et_xmpp_passwd);
+
+        findViewById(R.id.btn_xmpp_register).setOnClickListener(this);
+        findViewById(R.id.btn_xmpp_login).setOnClickListener(this);
+        findViewById(R.id.btn_xmpp_change_passwd).setOnClickListener(this);
+        findViewById(R.id.btn_xmpp_logout).setOnClickListener(this);
+
         Log.i(TAG,"开始vnc");
         Log.i(TAG,"结束vnc");
+
+        mXMPPManager=new XMPPManager();
         new Thread(new Runnable() {
             @Override
             public void run() {
-                //conServer();
+                mXMPPManager.conServer();
             }
         }).start();
 
     }
 
 
+    @Override
+    public void onClick(final View v) {
+        final String userName=mEtUserName.getText().toString();
+        final String passwd=mEtPasswd.getText().toString();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                switch (v.getId()){
+                    case R.id.btn_xmpp_register:
+                        String registerResult=mXMPPManager.register(userName,passwd);
+                        Log.i(TAG,"register result:"+registerResult);
+                        break;
+                    case R.id.btn_xmpp_login:
+                        boolean loginResult=mXMPPManager.login(userName,passwd);
+                        Log.i(TAG,"login result:"+loginResult);
+                        break;
+                    case R.id.btn_xmpp_logout:
+                        boolean logoutResult=mXMPPManager.logout();
+                        Log.i(TAG,"logout result:"+logoutResult);
+                        break;
+                    case R.id.btn_xmpp_change_passwd:
+                        boolean changePasswdResult=mXMPPManager.changePassword(passwd);
+                        Log.i(TAG,"changePasswd result:"+changePasswdResult);
+                        break;
+                    default:
+                        break;
 
+                }
+            }
+        }).start();
 
-
-
+    }
 }
--- app/src/main/java/com/folkcat/demo/xmpp/XMPPManager.java
+++ app/src/main/java/com/folkcat/demo/xmpp/XMPPManager.java
@@ -3,13 +3,29 @@ package com.folkcat.demo.xmpp;
 import android.util.Log;
 
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketCollector;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.SmackConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.AndFilter;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.filter.PacketIDFilter;
+import org.jivesoftware.smack.filter.PacketTypeFilter;
+import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.packet.Registration;
+import org.jivesoftware.smack.util.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collection;
+
+import static org.jivesoftware.smackx.filetransfer.FileTransfer.Error.connection;
 
 /**
  * Created by Tamas on 2016/9/18.
@@ -41,6 +57,126 @@ public class XMPPManager {
         }
         return false;
     }
+    public String register(String account, String password) {
+        if (mXMPPConnection == null)
+            return "0";
+        Registration reg = new Registration();
+        reg.setType(IQ.Type.SET);
+        reg.setTo(mXMPPConnection.getServiceName());
+        reg.setUsername(account);// 注意这里createAccount注册时，参数是username，不是jid，是“@”前面的部分。
+        reg.setPassword(password);
+        reg.addAttribute("android", "geolo_createUser_android");// 这边addAttribute不能为空，否则出错。所以做个标志是android手机创建的吧！！！！！
+        PacketFilter filter = new AndFilter(new PacketIDFilter(
+                reg.getPacketID()), new PacketTypeFilter(IQ.class));
+        PacketCollector collector = mXMPPConnection.createPacketCollector(filter);
+        mXMPPConnection.sendPacket(reg);
+        IQ result = (IQ) collector.nextResult(SmackConfiguration
+                .getPacketReplyTimeout());
+        // Stop queuing results
+        collector.cancel();// 停止请求results（是否成功的结果）
+        if (result == null) {
+            Log.e("RegistActivity", "No response from server.");
+            return "0";
+        } else if (result.getType() == IQ.Type.RESULT) {
+            return "1";
+        } else { // if (result.getType() == IQ.Type.ERROR)
+            if (result.getError().toString().equalsIgnoreCase("conflict(409)")) {
+                Log.e("RegistActivity", "IQ.Type.ERROR: "
+                        + result.getError().toString());
+                return "2";
+            } else {
+                Log.e("RegistActivity", "IQ.Type.ERROR: "
+                        + result.getError().toString());
+                return "3";
+            }
+        }
+    }
+    public boolean login(String a, String p) {
+        try {
+            if (mXMPPConnection== null)return false;
+            mXMPPConnection.login(a, p);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public  boolean changePassword(String pwd) {
+        try {
+            mXMPPConnection.getAccountManager().changePassword(pwd);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public void setPresence(int code) {
+        if (mXMPPConnection== null)
+            return;
+        Presence presence;
+        switch (code) {
+            case 0:
+                presence = new Presence(Presence.Type.available);
+                mXMPPConnection.sendPacket(presence);
+                Log.v("state", "设置在线");
+                break;
+            case 1:
+                presence = new Presence(Presence.Type.available);
+                presence.setMode(Presence.Mode.chat);
+                mXMPPConnection.sendPacket(presence);
+                Log.v("state", "设置Q我吧");
+                System.out.println(presence.toXML());
+                break;
+            case 2:
+                presence = new Presence(Presence.Type.available);
+                presence.setMode(Presence.Mode.dnd);
+                mXMPPConnection.sendPacket(presence);
+                Log.v("state", "设置忙碌");
+                System.out.println(presence.toXML());
+                break;
+            case 3:
+                presence = new Presence(Presence.Type.available);
+                presence.setMode(Presence.Mode.away);
+                mXMPPConnection.sendPacket(presence);
+                Log.v("state", "设置离开");
+                System.out.println(presence.toXML());
+                break;
+            case 4:
+                Roster roster = mXMPPConnection.getRoster();
+                Collection<RosterEntry> entries = roster.getEntries();
+                for (RosterEntry entry : entries) {
+                    presence = new Presence(Presence.Type.unavailable);
+                    presence.setPacketID(Packet.ID_NOT_AVAILABLE);
+                    presence.setFrom(mXMPPConnection.getUser());
+                    presence.setTo(entry.getUser());
+                    mXMPPConnection.sendPacket(presence);
+                    System.out.println(presence.toXML());
+                }
+                // 向同一用户的其他客户端发送隐身状态
+                presence = new Presence(Presence.Type.unavailable);
+                presence.setPacketID(Packet.ID_NOT_AVAILABLE);
+                presence.setFrom(mXMPPConnection.getUser());
+                presence.setTo(StringUtils.parseBareAddress(mXMPPConnection.getUser()));
+                mXMPPConnection.sendPacket(presence);
+                Log.v("state", "设置隐身");
+                break;
+            case 5:
+                presence = new Presence(Presence.Type.unavailable);
+                mXMPPConnection.sendPacket(presence);
+                Log.v("state", "设置离线");
+                break;
+            default:
+                break;
+        }
+    }
+    public  boolean logout(){
+        try {
+            mXMPPConnection.getAccountManager().deleteAccount();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
     public static void startVNC() {
         try {
             Log.i(TAG,"flag 1");
